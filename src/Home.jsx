184f7120a592125f410bb5b24cebbe10aa355512
Isabel Avery <isabelmavery@gmail.com>
2024-02-26 04:20:54 +0000

    style: header spacing cleanup

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import Menu from './Menu/Menu'
 function Home() {
   return (
       <div>
-        <h1 className="header" > Welcome {`<3`} </h1>
+        <h1 style={{ marginBottom: 20, marginTop: 0 }} className="header" > Welcome {`<3`} </h1>
         <Menu/>
       </div>
   );
