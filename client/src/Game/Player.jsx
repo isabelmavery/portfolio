410b2f4b44053be4c1d55008b2764817ccdb363e
isabelmavery <isabelmavery@gmail.com>
2024-05-06 20:37:47 -0400

    fix: fixes scroll on press space for game

--- client/src/Game/Player.jsx
+++ client/src/Game/Player.jsx
@@ -1,42 +1,47 @@
-import React, { useState } from 'react'
-import "./Game.css"
+import React, { useState } from "react";
+import "./Game.css";
 
 const KEY_CODES = {
-    SPACE_KEY: 32
-}
+  SPACE_KEY: 32,
+};
 
 const Player = React.forwardRef((props, ref) => {
-    const [jumping, setJumping] = useState(false)
+  const [jumping, setJumping] = useState(false);
 
-    const handleJump = () =>{
-        // don't need to trigger jump if already in the air
-        if (jumping) return 
+  const handleJump = (e) => {
+    e.preventDefault();
 
-        // turn on jumping animation
-        setJumping(true)
+    // don't need to trigger jump if already in the air
+    if (jumping) return;
 
-        // disable jumping animation
-        setTimeout(() => {
-            setJumping(false)
-        }, 700) 
-        // note, seconds must sync with css animation duration
+    // turn on jumping animation
+    setJumping(true);
+
+    // disable jumping animation
+    setTimeout(() => {
+      setJumping(false);
+    }, 700);
+    // note, seconds must sync with css animation duration
+  };
+
+  const handleMovement = (e) => {
+    switch (e.keyCode) {
+      case KEY_CODES.SPACE_KEY:
+        handleJump(e);
+        return;
     }
+  };
 
-    const handleMovement = (e) => {
-        switch (e.keyCode) {
-            case KEY_CODES.SPACE_KEY:
-                handleJump()
-            return
-        }
-    }
-
-    return (<div 
-        {...props}
-        tabIndex={0} 
-        className={jumping ? "jumping player" : "player"} 
-        onKeyDown={(e) => handleMovement(e)}
-        onClick={handleJump}
-        ref={ref}/>)
-})
+  return (
+    <div
+      {...props}
+      tabIndex={0}
+      className={jumping ? "jumping player" : "player"}
+      onKeyDown={(e) => handleMovement(e)}
+      onClick={handleJump}
+      ref={ref}
+    />
+  );
+});
 
 export default Player;
