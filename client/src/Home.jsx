dc91cafd84d7355ce7750906c00483f596ecee14
isabelmavery <isabelmavery@gmail.com>
2024-05-08 19:40:43 -0400

    feat: add waving hand

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,11 +1,18 @@
-import Menu from './Menu/Menu'
+import Menu from "./Menu/Menu";
+import Wave from "./assets/Wave";
 
 function Home() {
   return (
-      <div className="text-content primary-content">
-        <h1 style={{ marginBottom: 20, marginTop: 0 }} className="header" > Welcome {`<3`} </h1>
-        <Menu/>
+    <div className="text-content primary-content">
+      <div style={{ display: "flex", gap: 2 }}>
+        <h1 style={{ marginBottom: 20, marginTop: 0 }} className="header">
+          {" "}
+          Welcome
+        </h1>
+        <Wave />
       </div>
+      <Menu />
+    </div>
   );
 }
 
---
+++ client/src/assets/Wave.tsx
@@ -0,0 +1,255 @@
+import "./Wave.css";
+
+export default function Wave(props: { color?: string }) {
+  const { color = "#7297d6" } = props;
+  return (
+    <div className="waving-hand">
+      <svg
+        width="266"
+        height="276"
+        viewBox="0 0 266 276"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M101.893 232.359V232.359C72.1358 224.504 48.223 202.383 38.0786 173.327L37.2661 171"
+          stroke="#89BADF"
+          stroke-width="3"
+        />
+        <path
+          d="M75.7948 233.74V233.74C56.5483 228.632 41.0786 214.328 34.4817 195.539L33.9602 194.054"
+          stroke="#89BADF"
+          stroke-width="3"
+        />
+        <path
+          d="M98.7507 161.229C98.2231 161.299 96.8803 162.203 97.2802 161.851C97.9091 161.299 98.222 160.849 99.1347 160.727"
+          stroke="#80A1DA"
+          stroke-width="3"
+          stroke-linecap="round"
+        />
+        <path
+          d="M144.225 218.474L143.364 204.465L155.459 219.497L149.28 222.339L144.225 218.474Z"
+          fill="#7297D6"
+          stroke="#7297D6"
+          stroke-width="3"
+        />
+        <path
+          d="M88.1241 163.158C89.8427 162.929 91.8185 161.707 92.757 160.148C92.8683 159.964 94.8902 157.976 94.8315 157.983C94.0937 158.082 93.1685 161.297 92.6949 161.989C92.4465 162.352 91.9959 162.858 91.9321 163.293C91.7883 164.273 92.622 162.937 92.8563 162.778"
+          stroke="#80A1DA"
+          stroke-width="3"
+          stroke-linecap="round"
+        />
+        <rect
+          x="150.05"
+          y="181.765"
+          width="42"
+          height="19"
+          transform="rotate(-22.238 150.05 181.765)"
+          fill="#7297D6"
+        />
+        <rect
+          x="139.127"
+          y="201.684"
+          width="59.0148"
+          height="17.6614"
+          transform="rotate(-22.238 139.127 201.684)"
+          fill="#7297D6"
+        />
+        <rect
+          x="135.024"
+          y="190.125"
+          width="41.2095"
+          height="17.6614"
+          transform="rotate(-22.238 135.024 190.125)"
+          fill="#7297D6"
+        />
+        <rect
+          x="135.333"
+          y="192.927"
+          width="54.0706"
+          height="33.9778"
+          transform="rotate(-27.8691 135.333 192.927)"
+          fill="#7297D6"
+        />
+        <g filter="url(#filter0_d_210_259)">
+          <path
+            d="M185.874 98.683L184.125 112.032L184.122 112.302C184.081 116.78 185.03 121.212 186.902 125.281V125.281L187.135 125.853C190.418 133.925 191.395 142.75 189.957 151.345V151.345C189.183 155.97 189.105 160.685 189.725 165.333L189.845 166.228C190.934 174.391 193.909 182.189 198.535 189.003L199.036 189.742V189.742C201.37 193.181 200.162 197.886 196.461 199.776L163.289 216.708L149.746 223.011C148.698 223.498 147.488 223.491 146.446 222.992V222.992C145.311 222.448 144.512 221.384 144.306 220.142L142.268 207.868C141.164 201.215 137.678 195.192 132.46 190.92L124.132 184.102"
+            stroke="#80A1DA"
+            stroke-width="3"
+          />
+        </g>
+        <path
+          d="M167.192 129.601L193.542 80.5804"
+          stroke="#80A1DA"
+          stroke-width="20"
+          stroke-linecap="round"
+        />
+        <path
+          d="M130.632 119.421L102.803 54.5318"
+          stroke="#80A1DA"
+          stroke-width="20"
+          stroke-linecap="round"
+        />
+        <path
+          d="M150.688 118.512L152.598 50.663"
+          stroke="#80A1DA"
+          stroke-width="20"
+          stroke-linecap="round"
+        />
+        <circle
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.991211 -0.132291 -0.132291 -0.991211 107.673 151.492)"
+          fill="#80A1DA"
+        />
+        <g filter="url(#filter1_d_210_259)">
+          <circle
+            cx="144.846"
+            cy="145.697"
+            r="47.443"
+            transform="rotate(-7.60203 144.846 145.697)"
+            fill="#80A1DA"
+          />
+        </g>
+        <path
+          d="M90.4878 120.998L91.0661 121.701C97.518 129.548 99.8558 139.994 97.364 149.842V149.842L93.8967 165.437"
+          stroke="#80A1DA"
+          stroke-width="3"
+        />
+        <path
+          d="M128.945 99.219V99.219C133.543 103.212 140.576 102.4 144.142 97.4638L145.167 96.045"
+          stroke="#80A1DA"
+          stroke-width="3"
+        />
+        <path
+          d="M106.181 113.859L106.458 113.866C111.779 113.993 116.35 110.109 117.084 104.837V104.837"
+          stroke="#80A1DA"
+          stroke-width="3"
+        />
+        <path
+          d="M117.084 104.837L129.374 98.6572M144.606 95.6155L160.994 97.4637M171.205 102.154L188.453 110.445"
+          stroke="#80A1DA"
+          stroke-width="3"
+        />
+        <path
+          d="M156.831 96.506V96.506C159.698 103.18 167.305 106.422 174.105 103.867L174.443 103.74"
+          stroke="#80A1DA"
+          stroke-width="3"
+        />
+        <path
+          d="M98.5283 155.399C98.5452 155.91 98.0525 156.578 97.8587 157.018C97.3682 158.133 96.8512 159.232 96.3526 160.342C96.0537 161.008 95.8048 161.649 95.4558 162.285C95.3434 162.491 94.9238 163.567 95.4427 162.747C97.0299 160.241 98.4246 157.62 99.9712 155.092C100.244 154.645 99.3498 156.097 99.2853 156.201C97.8495 158.527 96.256 160.754 94.7443 163.03C94.3004 163.699 93.8088 164.354 93.3962 165.039C93.1726 165.41 93.0029 165.453 92.8878 165.97C92.8383 166.192 92.2506 168.893 92.4504 168.812C93.2104 168.501 93.8787 167.837 94.4664 167.283C95.461 166.346 96.4251 165.357 97.2629 164.276C97.7663 163.626 98.2701 162.977 98.8197 162.366C99 162.165 99.268 161.776 99.5538 161.704C100.016 161.587 99.2411 165.052 99.1331 165.361C99.0789 165.516 98.7995 166.008 98.8589 165.547C98.9037 165.198 99.1744 164.816 99.3297 164.506C99.4477 164.27 100.117 163.086 100.047 164.019C99.9665 165.092 99.4853 166.214 98.9812 167.152C98.8035 167.483 99.0537 167.065 99.1409 166.97C99.4088 166.678 99.5789 166.331 99.8189 166.017C99.8635 165.958 100.222 165.681 100.182 165.589C100.105 165.412 99.3645 165.722 99.2665 165.757C98.7257 165.951 97.5609 166.103 97.221 166.547"
+          stroke="#80A1DA"
+          stroke-width="3"
+          stroke-linecap="round"
+        />
+        <path
+          d="M90.5959 166.925C90.3099 166.706 89.966 166.397 89.6513 166.243C89.3205 166.081 90.3807 166.14 90.7457 166.092C91.1634 166.036 91.5811 165.98 91.9987 165.924"
+          stroke="#80A1DA"
+          stroke-width="3"
+          stroke-linecap="round"
+        />
+        <path
+          d="M97.064 125.95C97.2683 127.48 97.4261 128.924 97.9418 130.331C98.3633 131.481 98.9129 132.829 99.0769 134.058"
+          stroke="#80A1DA"
+          stroke-width="3"
+          stroke-linecap="round"
+        />
+        <path
+          d="M68.8845 168.275L131.48 178.638"
+          stroke="#80A1DA"
+          stroke-width="20"
+          stroke-linecap="round"
+        />
+        <path
+          d="M88.7106 163.951L88.9399 163.705C96.8747 155.212 99.9154 143.251 97 132V132"
+          stroke="#80A1DA"
+          stroke-width="3"
+        />
+        <path
+          d="M87.4688 162.956L89.7564 161.902C101.967 156.275 105.129 140.37 96 130.5V130.5"
+          stroke="#7297D6"
+          stroke-width="3"
+        />
+        <path
+          d="M68.7048 85.3144L115.44 135.072"
+          stroke="#80A1DA"
+          stroke-width="20"
+          stroke-linecap="round"
+        />
+        <defs>
+          <filter
+            id="filter0_d_210_259"
+            x="119.182"
+            y="98.488"
+            width="86.5491"
+            height="134.384"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_210_259"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_210_259"
+              result="shape"
+            />
+          </filter>
+          <filter
+            id="filter1_d_210_259"
+            x="93.397"
+            y="98.2482"
+            width="102.897"
+            height="102.897"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_210_259"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_210_259"
+              result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+}
