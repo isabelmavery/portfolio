88654f00b007e78bfdb693e3eb693524196281ae
isabelmavery <isabelmavery@gmail.com>
2024-05-08 19:57:06 -0400

    feat: automate month update

--- client/src/About/About.jsx
+++ client/src/About/About.jsx
@@ -1,5 +1,29 @@
 import "./About.css";
+
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export default function About() {
+  const now = new Date();
+  const nextMonth = new Date(
+    now.getFullYear(),
+    now.getMonth() + 1,
+    1
+  ).getMonth();
+  const monthName = MONTHS[nextMonth];
+
   return (
     <>
       <div className="about-me-blurb primary-content text-content">
@@ -13,7 +37,7 @@ export default function About() {
           I am from the chicago area originally (Evanston) and love a lot of
           things outside of work, though they seem to change from month to
           month. Right now I'm very into yoga, Chappel Roan and Neil Gaimen but
-          that could change by April :)
+          that could change by {monthName} :)
         </div>
       </div>
     </>
